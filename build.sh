c3ab88c9b8dabb0b59e0c0e5b038772de2084552
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-22 20:52:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,44 +1,21 @@
-#!/bin/bash
-
-apt install tmate -y
-
+#!/bin/sh -l -c                                                                                                                     
 mkdir -p /tmp/recovery
-
 cd /tmp/recovery
-
-# tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
-
-tg(){
-     bot_api=1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U	
-     your_telegram_id=$1 # No need to touch 
-     msg=$2 # No need to touch
-     curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
+apt install tmate -y
+apt install git -y
+git config --global user.name "Dazzler555"
+git config --global user.email "71560605+Dazzler555@users.noreply.github.com"
+git config --global color.ui false
+                                                                                                                                    tg(){
+        bot_api=1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U
+        your_telegram_id=$1
+        msg=$2
+        curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
 }
 
 id=1033360588
 
-tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Your Tmate XD" && tg $id "$send_shell"
+tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "semaphore starded :)" && tg $id "$send_shell"
 
-repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault 
-
-repo sync -j7 --force-sync
-
-repo sync
-
-repo sync
-
-git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
-
-rm -rf out
-
-. build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
-
-cd out/target/product/violet
-
-curl -sL https://git.io/file-transfer | sh
-
-./transfer wet *.zip
-
-./transfer wet recovery.img
-
-sleep 4500 && kill "$!"
+sleep 4500
+sleep 1500
